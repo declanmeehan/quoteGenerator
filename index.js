4a0e89df3734dbbc5293c8420b51ebb1bcb5069b
Declan <declan.meehan@gmail.com>
2018-02-10 00:34:00 -0600

    npm express. oh. my.

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-$("#quoteGETJSON").click(function() {
-  $.getJSON(
-    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
-  )
-    .done(update)
-    .fail(handleErr);
+/* global $, jQuery */
+
+jQuery.ajaxPrefilter(function(options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    options.url = "https://cors-anywhere.herokuapp.com/" + options.url;
+  }
 });
 
-function update(response) {
-  $("#log").prepend("<pre>" + $("response").html() + "</pre>");
-  $("#response").html(JSON.stringify(response));
+const uri =
+  "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+
+$(document).ready(function() {
+  $("#quoteGETJSON").on("click", function() {
+    fetch(uri).then(response => console.log(response.json));
+  });
+});
+
+function update(quote) {
+  $(".mesage").html(JSON.stringify(quote));
 }
 
-// $(function() {
+//     .done(update)
+//     .fail(handleErr);
+// });
+
+// function update(response) {
+//   $("#log").prepend("<div>" + $("response").html() + "</div>");
+//   $("#response").html(JSON.stringify(response));
+// }
+
+// function handleErr(textStatus, err) {
+//   console.log("Request Failed: " + textStatus + ", " + err);
+// }
+
+// $(function getting() {
 //   var $quote = $("#quotes");
 
 //   $.ajax({
 //     type: "GET",
 //     url:
 //       "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json",
-//     success: function(quotes) {
-//       $.each(quotes, function(i, quote) {});
-//       $quote.append("<p>works</p>");
+//     success: function(succ) {
+//       $response.append('<p> "works');
 //     }
 //   });
 // });
